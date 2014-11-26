78c63ccf095aa29ebff38a1a9f5032714070af15
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-26 16:38:45 +0000

    TDD base_title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "VotesMe"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "VotesMe | Home"
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "VotesMe | Help"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "VotesMe | Contact"
+    assert_select "title", "#{@base_title} | Contact"
   end
 
   test "should get tos" do
     get :tos
     assert_response :success
-    assert_select "title", "VotesMe | TOS"
+    assert_select "title", "#{@base_title} | TOS"
   end
 
 end
