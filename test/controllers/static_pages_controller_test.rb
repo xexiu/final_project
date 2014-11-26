eb49f1f94e7dd99ae66e5c3e3f73d99dccfdd185
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-26 15:49:42 +0000

    TDD + Refactor - green

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "VotesMe | Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "VotesMe | Help"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "VotesMe | Contact"
   end
 
   test "should get tos" do
     get :tos
     assert_response :success
+    assert_select "title", "VotesMe | TOS"
   end
 
 end
