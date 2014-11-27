1d14de50d389d8aa327ae8a9229c46c19492c57f
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-27 20:40:44 +0000

    TDD Email Uniqness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,4 +48,11 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
