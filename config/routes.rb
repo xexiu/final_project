d6a890051872d300cd0df085083c0eeea421759e
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-27 18:50:12 +0000

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  'static_pages/help'
-  get  'static_pages/contact'
-  get  'static_pages/tos'
+  get 'help' => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
+  get 'tos' => 'static_pages#tos'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
