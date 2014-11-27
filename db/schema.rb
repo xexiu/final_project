26312fab496c0d528e098936c33df57605763ba3
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-27 20:58:27 +0000

    Added Password Digest to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127201919) do
+ActiveRecord::Schema.define(version: 20141127205749) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
