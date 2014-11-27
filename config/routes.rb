b59312ed07bbf466018eefa5707d5f47fb8949fa
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-27 19:33:34 +0000

    Fixed sign up button --> link

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
   get 'tos'     => 'static_pages#tos'
+
+  get 'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
