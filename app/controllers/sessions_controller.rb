08c4188e85741d9e14991102e9ea0c76c3920ec6
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-28 23:57:11 +0000

    Users Log In Flash Message incorrect - bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
