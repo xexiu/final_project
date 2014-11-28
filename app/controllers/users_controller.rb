2e2f99f730451fe31a0997ee9afb43fb24bfbea5
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-28 22:34:40 +0000

    Set Time Out for Success Message User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # flash[:success] = "Welcome to the VotesMe!"
+      flash[:success] = "Welcome #{@user.name}, to VotesMe!"
       redirect_to @user
     else
       render 'new'
