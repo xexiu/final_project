d590c122a9e61b61b7dceb6648c93ac6d10811c9
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-28 22:20:24 +0000

    Polling User Sign Uo p

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      # flash[:success] = "Welcome to the VotesMe!"
+      redirect_to @user
     else
       render 'new'
     end
