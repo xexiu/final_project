fdfb131ab77cd01e01963e810fc5c634052fc753
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-29 16:48:33 +0000

    Set checkbox to true by default

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       remember user
       redirect_to user
     else
