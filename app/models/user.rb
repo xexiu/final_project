91ccd9a46aea5198bf432f15896b6d68825397e4
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-29 16:55:26 +0000

    Finished log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   #  Some database adapters use case-sensitive indices, considering the strings “Foo@ExAMPle.CoM” and “foo@example.com” to be distinct, but our application treats those addresses as the same. To avoid this incompatibility, we’ll standardize on all lower-case addresses, converting “Foo@ExAMPle.CoM” to “foo@example.com” before saving it to the database.
-
   attr_accessor :remember_token
 
   before_save { self.email = email.downcase }
