2fa9c9d0936b34dbd035b0eb77e4b3ef326eb81a
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-29 13:37:29 +0000

    Dropdown on Index

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, :size => '150', alt: user.name, class: "gravatar img-thumb")
+    image_tag(gravatar_url, :size => '40', alt: user.name, class: "gravatar img-thumb")
   end
 end
