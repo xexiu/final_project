8b24070ed22bb5fffd19fac24cd71a355697d0f8
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-29 15:25:09 +0000

    TDD User Login Test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
