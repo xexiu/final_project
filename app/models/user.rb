57abcf9cd552f8d8a9fa380e0d6b5bfbd50101f0
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-12-01 00:39:03 +0100

    Revert "Add password resets & email configuration"
    This reverts commit ea10156b6c7409d78b748357357e60d13272f2b1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,11 +66,6 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver
   end
 
-  # Returns true if a password reset has expired.
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
-  end
-
   private
 
   # Converts email to all lower-case.
