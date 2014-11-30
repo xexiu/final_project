c6dc7e08a06739149441e9befceb64d77ce4ef6c
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-11-30 22:11:00 +0000

    Added forgot password to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141130204051) do
+ActiveRecord::Schema.define(version: 20141130220536) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20141130204051) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
