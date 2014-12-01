6c0d72f54829f3bdb0b6ab065eb2360178843e7a
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-01 14:27:23 +0000

    TDD For Entry Validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,5 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 600 }
 end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    # This code is not idiomatically correct.
+    @entry = Entry.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @entry.valid?
+  end
+
+  test "user id should be present" do
+    @entry.user_id = nil
+    assert_not @entry.valid?
+  end
+
+  test "content should be present " do
+    @entry.content = "   "
+    assert_not @entry.valid?
+  end
+
+  test "content should be at most 600 characters" do
+    @entry.content = "a" * 601
+    assert_not @entry.valid?
+  end
+
 end
