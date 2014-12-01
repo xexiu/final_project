eafbb80439de9071dcf2c3a48541377f1a401970
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-01 15:22:42 +0000

    style tabs on home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :entries
+  has_many :entries, dependent: :destroy
   #  Some database adapters use case-sensitive indices, considering the strings “Foo@ExAMPle.CoM” and “foo@example.com” to be distinct, but our application treats those addresses as the same. To avoid this incompatibility, we’ll standardize on all lower-case addresses, converting “Foo@ExAMPle.CoM” to “foo@example.com” before saving it to the database.
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
