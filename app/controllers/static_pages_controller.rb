d6efbdedd3aae4d88d4f50f81e0f6a51ded960b9
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-02 11:52:04 +0000

    Fixed transition tabs profile

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
-    @entries_all = Entry.all.order(created_at: :desc).paginate(page: params[:page],  :per_page => 5)
+    @entries_all = Entry.all.order(created_at: :desc).limit(100).paginate(page: params[:page],  :per_page => 5)
     @entries_random = Entry.order("RANDOM()").limit(100).paginate(page: params[:page],  :per_page => 5)
     # @entries = @user.entries.paginate(page: params[:page],  :per_page => 10)
     # @entries = Entry.all
