f71da93ef22da1de607bf2914f5bc4fecda37df9
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-02 14:57:22 +0000

    Styled Show Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,8 +4,8 @@ class Entry < ActiveRecord::Base
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 55 }
   validates :content, presence: true, length: { minimum: 300, maximum: 600 }
-  validates :img, presence: true
-  has_attached_file :img
+
+  has_attached_file :img, :default_url => "/images/missing.png"
   validates_attachment :img,
   :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
   validates_attachment_size :img, :less_than => 2.megabytes
