c6f8e5b31f06b687d5df0923575d12f35fc77e92
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-03 00:41:28 +0000

    Fixed index - guest

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
-    @user = User.new
+    @user = User.new unless logged_in?
     @entries_all = Entry.all.order(created_at: :desc).limit(100).paginate(page: params[:page],  :per_page => 5)
     @entries_random = Entry.order("RANDOM()").limit(100).paginate(page: params[:page],  :per_page => 5)
   end
