87bd7cd6d0446b8c7b9caa008be063d7bd2a460f
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-03 09:48:14 +0000

    Fixed footer and arrow tab on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,4 +14,25 @@ class StaticPagesController < ApplicationController
 
   def tos
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
