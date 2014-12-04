10daf1564a5eac34fe22ef0900dd97e56ee9409d
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-04 15:34:42 +0000

    Will add total_votes count

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
     @user = User.new unless logged_in?
     @entries_all = Entry.all.order(created_at: :desc).limit(100).paginate(page: params[:page],  :per_page => 5)
     @entries_random = Entry.order("RANDOM()").limit(100).paginate(page: params[:page],  :per_page => 5)
+    @entries_most_voted = 
   end
 
   def help
