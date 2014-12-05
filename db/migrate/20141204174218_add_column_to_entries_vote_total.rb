a21875691e05fa005b18622538ca2718be9ca6da
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-05 09:46:45 +0000

    Fixed Column add votes total

--- db/migrate/20141204174218_add_column_to_entries_vote_total.rb
+++ db/migrate/20141204174218_add_column_to_entries_vote_total.rb
@@ -1,5 +1,5 @@
 class AddColumnToEntriesVoteTotal < ActiveRecord::Migration
   def change
-    add_column :entries, :vote_total, :integer, default: 0c
+    add_column :entries, :vote_total, :integer, default: 0
   end
 end
