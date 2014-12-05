649974182f956886cd561cd127d32286bcbd4b84
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-05 12:52:48 +0000

    Add validation uniqueness title entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,8 +3,8 @@ class Entry < ActiveRecord::Base
   belongs_to :user
   # default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 55 }
-  validates :content, presence: true, length: { minimum: 200, maximum: 600 }
+  validates :title, presence: true, uniqueness: true,  length: { maximum: 55 }
+  validates :content, presence: true, length: { minimum: 55, maximum: 800 }
 
   has_attached_file :img, :styles => { :small  => "150x150#", :medium => "300x300#", :thumb => "100x100" }, :default_url => "/images/missing.png"
   validates_attachment :img,
