fe1cd73c03d6bbfe824b75e5a962ad57a215106e
Sergiu Mironescu <sergiu.mironescu@gmail.com>
2014-12-09 20:58:20 +0000

    Finished Ajax Follow

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,13 @@
 class StaticPagesController < ApplicationController
+
   def home
     @user = current_user
     @user = User.new unless logged_in?
     @entries_all = Entry.all.order(created_at: :desc).limit(100).paginate(page: params[:page],  :per_page => 10)
     @entries_random = Entry.order("RANDOM()").limit(100).paginate(page: params[:page],  :per_page => 10)
-
     @entries_most_voted = Entry.all.order("vote_total DESC").limit(100).paginate(page: params[:page],  :per_page => 10)
-
      @entries_own = @user.entries.order(created_at: :desc).paginate(page: params[:page],  :per_page => 10)
-
+     @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)
   end
 
   def help
--- app/models/user.rb
+++ app/models/user.rb
@@ -98,6 +98,14 @@ class User < ActiveRecord::Base
     following.include?(other_user)
   end
 
+  # Returns a user's status feed.
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Entry.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
   private
 
   # Converts email to all lower-case.
