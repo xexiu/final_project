6ad8a2e4c90dfc90e8f4b3d05b6fb5064dd59ff6
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-12-14 20:13:16 +0100

    Fixed search input for heroku pg v2

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -12,7 +12,7 @@ class Entry < ActiveRecord::Base
   validates_attachment_size :img, :less_than => 2.megabytes
 
   def self.search(query)
-    where("title like ? OR cast(id as text) like ?", "%#{query}%", "%#{query}%") unless Rails.env.development?
+    where("title like ? OR cast(id as text) like ?", "%#{query}%", "%#{query}%") unless Rails.env.production?
     else
     where("title like ? OR id like ?", "%#{query}%", "%#{query}%")
   end
